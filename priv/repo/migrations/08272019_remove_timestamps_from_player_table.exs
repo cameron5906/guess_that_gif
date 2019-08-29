da5b65f9f23308eacdca1aebf11e1f4cd00d1232
ccooper <ccooper@cars.com>
2019-08-29 10:34:38 -0400

    Fixed a non working migration file

--- priv/repo/migrations/08272019_remove_timestamps_from_player_table.exs
+++ priv/repo/migrations/08272019_remove_timestamps_from_player_table.exs
@@ -1,10 +1,10 @@
 defmodule Repo.Migrations.RemoveTimestampsFromPlayer do
   use Ecto.Migration
 
-  def up do
-    alter table "player" do
-      remove "inserted_at"
-      remove "updated_at"
+  def change do
+    alter table :player do
+      remove :inserted_at
+      remove :updated_at
     end
   end
 end
