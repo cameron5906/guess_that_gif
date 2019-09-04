9d2b76ca614e001789779ba01aa85c92e81a4bcd
Cameron Cooper <cameron@cameron.pw>
2019-09-03 22:53:42 -0400

    Move off reducers

--- assets/src/Commands.elm
+++ assets/src/Commands.elm
@@ -4,33 +4,6 @@ import Http exposing(..)
 import Decoders exposing(..)
 import Encoders exposing(..)
 
-update_remove_latest_guesses: Model -> Model
-update_remove_latest_guesses model =
-    {
-        model |
-        players = List.map(
-        \ply -> 
-            if model.current_time - ply.guess_time > 2 then
-                {ply | guess = ""}
-            else
-                ply
-        ) model.players
-    }
-
-update_player_guess: Model -> String -> String -> Model
-update_player_guess model username guess =
-    {
-        model |
-        players = 
-            List.map(
-                \ply ->
-                    if ply.name == username then
-                        {ply | guess = guess, guess_time = model.current_time}
-                    else
-                        ply
-            ) model.players
-    }
-
 create_game_request: String -> Cmd Msg
 create_game_request username =
     Http.post
